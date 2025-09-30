4c4bc2b1b8a4b10978e36693e231e28c3a572bf1
anjali naineni <anjalinaineni@anjalis-MacBook-Air-2.local>
2025-09-30 18:18:30 -0400

    a2 CSS and Bootstrap

--- app/(kambaz)/Account/Navigation.tsx
+++ app/(kambaz)/Account/Navigation.tsx
@@ -5,20 +5,17 @@ export default function AccountNavigation() {
     <div id="wd-account-navigation" className="list-group rounded-0">
       <Link 
         href="/Account/Signin" 
-        className="list-group-item list-group-item-action border-0 text-dark fw-bold border-start border-dark border-3"
-      >
+        className="list-group-item list-group-item-action border-0 text-dark fw-bold border-start border-dark border-3">
         Signin
       </Link>
       <Link 
         href="/Account/Signup" 
-        className="list-group-item list-group-item-action border-0 text-danger"
-      >
+        className="list-group-item list-group-item-action border-0 text-danger">
         Signup
       </Link>
       <Link 
         href="/Account/Profile" 
-        className="list-group-item list-group-item-action border-0 text-danger"
-      >
+        className="list-group-item list-group-item-action border-0 text-danger">
         Profile
       </Link>
     </div>
